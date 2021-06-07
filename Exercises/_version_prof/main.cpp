eab59ae3268ffc5b985b453490c723257160067d
chosson <chosson@users.noreply.github.com>
2021-06-07 16:10:54 -0400

    Added in-class answers

--- Exercises/_version_prof/main.cpp
+++ Exercises/_version_prof/main.cpp
@@ -29,7 +29,10 @@ using namespace iter;
 void runListExample() {
 	{
 		List<int> alexanderThibault = {1, 3, 4};
-		auto it = ++alexanderThibault.begin(); // Faire ++ sur un itérateur le fait avancer, donc faire ++ sur le résultat de begin() (le premier élément) nous amène au deuxième.
+		for (auto it = alexanderThibault.begin(); it != alexanderThibault.end(); ++it)
+			cout << *it << " ";
+		cout << "\n";
+		auto it = ++alexanderThibault.begin(); // Faire ++ sur un itÃ©rateur le fait avancer, donc faire ++ sur le rÃ©sultat de begin() (le premier Ã©lÃ©ment) nous amÃ¨ne au deuxiÃ¨me.
 		alexanderThibault.insert(it, 2);
 		cout << alexanderThibault << "\n";
 
@@ -45,7 +48,7 @@ void runListExample() {
 	{
 		List<int> michelleCardinal = {69, 420};
 		cout << michelleCardinal << "\n";
-		michelleCardinal.erase(--michelleCardinal.end()); // On ne peut pas erase le end(), car cet élément n'existe pas (il est après la fin de la liste). C'est pour cela que je fait -- sur end() pour avoir un itérateur sur le dernier élément. C'est pas mal la convention en C++.
+		michelleCardinal.erase(--michelleCardinal.end()); // On ne peut pas erase le end(), car cet Ã©lÃ©ment n'existe pas (il est aprÃ¨s la fin de la liste). C'est pour cela que je fait -- sur end() pour avoir un itÃ©rateur sur le dernier Ã©lÃ©ment. C'est pas mal la convention en C++.
 		cout << michelleCardinal << "\n";
 		michelleCardinal.pop_back();
 		cout << michelleCardinal << "\n";
@@ -53,17 +56,37 @@ void runListExample() {
 }
 
 
+void runIteratorExample() {
+	vector<int> v = {2, 1, 42, 12, 69};
+	//auto it = v.begin();
+	//int v[] = {2, 1, 42, 12, 69};
+	//int* begin = v;
+	auto begin = v.begin();
+	//int* end = begin + 5;
+	auto end = v.end();
+	for (auto p = begin; p != end; ++p)
+		cout << *p << " ";
+}
+
+
 int main() {
 	runListExample(); cout << "\n\n\n\n";
+	//runIteratorExample(); cout << "\n\n\n\n";
 
-	const List<int> foo = {1, 2, 3};
-	auto it = foo.begin();
-	cout << *it << "\n";
-	++it;
-	cout << *it << "\n";
-	--it;
-	cout << *it << "\n";
-	for (auto e : foo)
-		cout << e << " ";
+	// Soit f() une fn logarithmique sur taille de v
+	// Soit g() une fn linÃ©aire sur taille de v
+	// Soit n la taille du tableau
+	vector<int> v = {2, 1, 42, 12, 69}; // O(n)
+	for (auto&& e1 : v) {   // O(n)
+		int cnt = 0;        // O(1)
+		for (auto&& e2 : v) // O(n)
+			if (f(e2, v))   // O(log n)
+				g(cnt, v);  // O(n)
+		cout << cnt;        // O(1)
+	}
+	// O(n + n(1 + n(log n + n) + 1))
+	// O(n + n(1 + nlog n + nÂ² + 1))
+	// O(n + n(nÂ²))
+	// O(nÂ³)
 }
 
